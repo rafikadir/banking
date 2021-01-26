9f673306b77657064a53a7c785811fa89500b2b5
rafikadir <54663767+rafikadir@users.noreply.github.com>
2021-01-26 20:22:42 +0600

    Add withdraw

--- script.js
+++ script.js
@@ -11,16 +11,25 @@
     // Deposit Buttom event handler
     const depositBtn = document.getElementById('deposit');
     depositBtn.addEventListener('click', function(){
-        const depositAmount = document.getElementById("depositAmount").value;
-        const depositNumber = parseFloat(depositAmount);
+        const depositNumber = getInputNumber("depositAmount");
 
-        //Call Function
         updateText("currentDepo",depositNumber)
         updateText("balance",depositNumber);
 
-        //Value Clean in Input
         document.getElementById("depositAmount").value = "";
     });
+
+    // Withdraw Event Button handler
+    const withdrawBtn = document.getElementById('withdraw');
+    withdrawBtn.addEventListener('click',function(){
+        const withdrawAmountNumber = getInputNumber("withdrawAmount");
+    });
+
+    function getInputNumber(id){
+        const amount = document.getElementById(id).value;
+        const amountNumber = parseFloat(amount);
+        return amountNumber;
+    }
 
     function updateText(id,depositNumber){
         const currentBalance = document.getElementById(id).innerText;
@@ -28,20 +37,12 @@
         const totalBalance = depositNumber + balanceNumber;
         document.getElementById(id).innerText = totalBalance;
     };
-    
-    //Withdraw Balance
-    const withdrawBtn = document.getElementById('withdraw');
-    withdrawBtn.addEventListener('click',function(){
-        const withdrawAmount = document.getElementById("withdrawAmount").value;
-        const withdrawAmountNumber = parseFloat(withdrawAmount);
+  
         
-        const currentWithdraw = document.getElementById("currentWithdraw").innerText;
-        const currentWithdrawNumber = parseFloat(currentWithdraw);
-        const totalWithdraw = withdrawAmountNumber + currentWithdrawNumber;
-        document.getElementById("currentWithdraw").innerText = totalWithdraw;
+    //     const currentWithdraw = document.getElementById("currentWithdraw").innerText;
+    //     const currentWithdrawNumber = parseFloat(currentWithdraw);
+    //     const totalWithdraw = withdrawAmountNumber + currentWithdrawNumber;
+    //     document.getElementById("currentWithdraw").innerText = totalWithdraw;
 
-        document.getElementById("withdrawAmount").value = "";
-    });
+    //     document.getElementById("withdrawAmount").value = "";
 
-
-    
